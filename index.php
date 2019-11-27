bc15d782d3d78363c61a40a6d551470f0afa3c89
Lucas Muniz <lucas.s.lima@pdmfc.com>
2019-11-27 13:30:10 +0000

    added select from DB

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $dbpass = '';
 
 try {
     $pdo = new PDO($dsn, $dbuser, $dbpass);
-    echo "Conection Estabilished.";
+    echo "Connection to the database successfully estabilished!<br><br>";
 
 } catch(PDOException $e) {
     echo "Failed: ".$e->getMessage();
@@ -19,15 +19,31 @@ $email = addslashes(@$_POST["email"]);
 $password = addslashes(md5(@$_POST["password"]));
 
 $sql = "INSERT INTO users (name, email, password) values ('$name', '$email', '$password')";
+$sqlt = "SELECT * FROM users";
+$sqlt = $pdo->query($sqlt);
 
-if(isset($_POST) && $_POST){
+
+
+if(isset($_POST['submit'])){
     $sql = $pdo->query($sql);
+    echo "<meta http-equiv='refresh' content='0'>";
+}
+
+if($sqlt->rowCount() > 0){
+    foreach($sqlt->fetchAll() as $user){
+        echo "Name: ". $user["name"]. "<br> E-mail - ".$user["email"]."<br><br>";
+    }
+}else{
+    echo "There are no registered users.";
 }
 ?>
 
 <form method="POST">
+    <label>Name:</label>
     <input type="text" name="name">
-    <input type="text" name="email">
+    <label>E-mail:</label>
+    <input type="email" name="email">
+    <label>Password:</label>
     <input type="text" name="password">
     <input type="submit" name="submit">
 </form>
