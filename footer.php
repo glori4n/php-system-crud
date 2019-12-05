800875433fad4deee66016a9f45dd3c1cc014fa2
Lucas <47852219+glori4n@users.noreply.github.com>
2019-12-05 15:38:54 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <br>
 <br>
-<footer style="text-align:center">This snippet was made by Glori4n (<a href="https://glori4n.com" target="new">https://glori4n.com</a>)</footer>
+<footer style="text-align:center">This snippet was made by © Glori4n (<a href="https://glori4n.com" target="new">https://glori4n.com</a>)</footer>
